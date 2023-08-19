efc97a376f5474cde6d268672a6a6feffb95f8af
Jonatas Arlindo Pedroso <jonatasarlindo@hotmail.com>
2023-08-19 11:50:25 -0300

    Melhorias Aula 2

--- auladois/ExercicioUm.java
+++ auladois/ExercicioUm.java
@@ -1,35 +1,39 @@
 package auladois;
 
 import javax.swing.*;
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 public class ExercicioUm {
     public static void main(String[] args) {
         List<Float> floatList = new ArrayList<>();
+        Random random = new SecureRandom();
+        int maxRandom = 1000;
 
         for (int i = 0; i < 10; i++) {
-            floatList.add(Float.parseFloat(JOptionPane.showInputDialog("Informe um número Float: ")));
+            floatList.add(Float.parseFloat(String.valueOf(random.nextInt(maxRandom))));
         }
 
         //VALOR MAIOR
         floatList.stream()
                 .mapToDouble(Float::floatValue)
                 .max()
-                .ifPresent(ExercicioUm::printValue);
+                .ifPresent(value -> ExercicioUm.printValue("MAIOR VALOR", value));
 
         //VALOR MENOR
         floatList.stream()
                 .mapToDouble(Float::floatValue)
                 .min()
-                .ifPresent(ExercicioUm::printValue);
+                .ifPresent(value -> ExercicioUm.printValue("MENOR VALOR", value));
 
         //MEDIA DO VALOR
         floatList.stream()
                 .mapToDouble(Float::floatValue)
                 .average()
-                .ifPresent(ExercicioUm::printValue);
+                .ifPresent(value -> ExercicioUm.printValue("MEDIA DO VALOR", value));
 
         //VETOR COMPLETO
         JOptionPane.showMessageDialog(null, floatList.stream()
@@ -37,7 +41,7 @@ public class ExercicioUm {
                 .collect(Collectors.joining()));
     }
 
-    private static void printValue(double value) {
-        JOptionPane.showMessageDialog(null, value);
+    private static void printValue(String message, double value) {
+        JOptionPane.showMessageDialog(null, String.format("%s - %.2f", message, (float) value));
     }
 }
